38b8db03c5357811353f295faa96696f895f2f31
Yash Aslekar <88288316+Czech-Knight@users.noreply.github.com>
2024-09-02 10:01:32 +0530

    Add files via upload

---
+++ fullstack-admin-master_new/server/models/File.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const FileSchema = new mongoose.Schema({
+  filename: String,
+  originalName: String,
+  mimetype: String,
+  size: Number,
+  uploadDate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('File', FileSchema);
